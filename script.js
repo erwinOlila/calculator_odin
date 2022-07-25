3cdf59704466cec7091255fa997b39e5fe7a61fc
erwinOlila <ce.olila@gmail.com>
2022-07-25 20:27:02 +0800

    worked on special operators

--- script.js
+++ script.js
@@ -6,8 +6,41 @@ characters.forEach(char => {
 })
 
 function setDisplay(e) {
-    if (display.textContent.length < 15 && !this.classList.contains('operator') && !this.classList.contains('special')) {
-        display.textContent += this.textContent;
-        console.log(display.textContent.length);
+    if (!this.classList.contains('operator') && !this.classList.contains('special')) {
+        if (display.textContent.length < 18){
+            if(this.textContent.includes('.')) {
+                if (!display.textContent.includes('.')) {
+                    display.textContent += this.textContent;
+                }
+            } else {
+                display.textContent += parseFloat(this.textContent);
+                console.log(display.textContent.length);
+            }
+        } 
+    } else if (this.classList.contains('special')) {
+        if (this.textContent.includes('C')) {
+            display.textContent = '';
+        } else if(this.textContent.includes('+/-')) {
+            display.textContent *= -1;
+        } else {
+            console.log('divide');
+            display.textContent /= 100;
+        }
     }
+    // if (display.textContent.length < 18) {
+    //     if (!this.classList.contains('operator') && !this.classList.contains('special')) {
+    //         display.textContent += parseFloat(this.textContent);
+    //         console.log(display.textContent.length);
+    //     }else if (this.classList.contains('special')) {
+    //         if (this.textContent.includes('C')) {
+    //             display.textContent = '';
+    //         } else if(this.textContent.includes('+/-')) {
+    //             display.textContent *= -1;
+    //         } else {
+    //             console.log('divide');
+    //             display.textContent /= 100;
+    //         }
+    //     }
+    // } 
 }
+    
